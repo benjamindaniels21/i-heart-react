f59996be1415a9c56d2f3d4087ec1cc1e2a5df92
Ben Daniels <benjamindaniels21@gmail.com>
2022-09-12 19:43:09 -0400

    refactored components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import Heartlogo from "./heart.svg";
 import "./App.css";
-
-const Header = () => {
-  return (
-    <div className="App-header">
-      <h1>I ❤️ React</h1>
-    </div>
-  );
-};
+import Header from "./components/Header";
+import Heart from "./components/Heart";
 
 const message = "cool cud";
-const Heart = (props) => {
-  return (
-    <div className="heart">
-      <img className="heart-img" src={Heartlogo} alt="heart" />
-      <p className="heart-message">{props.msg}</p>
-    </div>
-  );
-};
 
 function App() {
   return (
---
+++ src/components/HeartsList.js
@@ -0,0 +1,12 @@
+import Heartlogo from "./heart.svg";
+
+const Heart = (props) => {
+  return (
+    <div className="heart">
+      <img className="heart-img" src={Heartlogo} alt="heart" />
+      <p className="heart-message">{props.msg}</p>
+    </div>
+  );
+};
+
+export default Heart;
